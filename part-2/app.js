eaf1835b8b8b393047b49cb00bc4eaa46d11b763
yuanyanmin <1414011709@qq.com>
2022-05-11 22:52:20 +0800

    learning source-map devServer 热加载

--- part-2/app.js
+++ part-2/app.js
@@ -1,1 +1,38 @@
-console.log('hello')
+import './style.css'
+import './input.js'
+
+class A {
+    constructor () {
+        this.str = 'hello webpack'
+    }
+
+    sayHello() {
+        console.log(this.str);
+    }
+}
+
+const a = new A()
+a.sayHello()
+
+// fetch('/api/hello').then(res => res.text()).then(res => {
+//     console.log(res);
+// })
+
+const button = document.createElement('button')
+
+button.textContent = '添加'
+
+button.addEventListener('click', () => {
+    const div = document.createElement('div')
+    div.classList.add('square')
+    document.body.appendChild(div)
+})
+
+
+document.body.appendChild(button)
+
+if (module.hot) {
+    module.hot.accept('./input.js', () => {
+        
+    })
+}
--- part-2/webpack.config.js
+++ part-2/webpack.config.js
@@ -1,10 +1,57 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path')
 
 module.exports = {
+    // mode: 'production',
     mode: 'development',
     entry: './app.js',
-    devtool: 'eval',
+    output: {
+        publicPath: '/',
+        clean: true
+    },
+    // devtool: 'cheap-source-map',
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+
+                }
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            }
+        ]
+    },
     plugins: [
-        new HtmlWebpackPlugin()
-    ]
+        new HtmlWebpackPlugin({
+            template: './index.html'
+        })
+    ],
+    devServer: {
+        hot: true,
+        liveReload: true,
+        static: path.resolve(__dirname, './dist'),
+        compress: false,
+        port: 3000,
+        host: '0.0.0.0',
+        headers: {
+            'X-Access-Token': '1212'
+        },
+        proxy: {
+            '/api': {
+                target: 'http://localhost:9000/',
+
+            }
+        },
+        // https: true
+        // http2: true
+        historyApiFallback: true
+    }
 }
